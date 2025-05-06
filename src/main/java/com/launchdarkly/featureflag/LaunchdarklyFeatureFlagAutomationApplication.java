9cf172e69cf530d056047933ee4f5d6ca035b2be
omohanteja <mohan.teja131@gmail.com>
2025-05-06 11:36:07 +0530

    initial project setup

---
+++ src/main/java/com/launchdarkly/featureflag/LaunchdarklyFeatureFlagAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.launchdarkly.featureflag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchdarklyFeatureFlagAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchdarklyFeatureFlagAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchdarkly/featureflag/LaunchdarklyFeatureFlagAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchdarkly.featureflag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchdarklyFeatureFlagAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
